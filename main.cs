ade209df4c888151c55422eaf8df09b6a7767c1e
Kostiantyn Polishko <kostantinpolishko@gmail.com>
2023-12-18 05:45:34 +0200

    Add project files.

---
+++ main.cs
@@ -0,0 +1,10 @@
+﻿namespace C__OOP_HW006_class_inherit_r00
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
